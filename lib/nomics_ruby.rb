6607213abab50770dce66de0bdf79405947c224b
Jonic Linley <jonic@100yen.co.uk>
2021-10-17 15:59:04 +0100

    Init Gem

---
+++ lib/nomics_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "nomics_ruby/version"
+
+module NomicsRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nomics_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NomicsRuby
+  VERSION = "0.1.0"
+end
